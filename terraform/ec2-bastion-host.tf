be741355f158412a6cdb0adfa8b2fd547b10fd54
ayanchasrinivas <ayanchasrinivas001@gmail.com>
2026-06-07 14:30:18 +0530

    Updated Logic

--- terraform/ec2-bastion-host.tf
+++ terraform/ec2-bastion-host.tf
@@ -1,56 +1,71 @@
+# Generate a key and registers it in AWS.
+
 resource "tls_private_key" "bastion_key" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "bastion_keypair" {
-    key_name = "srinivas-project-keypair"
-    public_key = tls_private_key.bastion_key.public_key_openssh
+  key_name   = "bastion-key"
+  public_key = tls_private_key.bastion_key.public_key_openssh
 }
+
+
+# Save the private key locally
 
 resource "local_file" "bastion_private_key" {
-    content = tls_private_key.bastion_key.private_key_pem
-    filename = "srinivas-project-keypair.pem"
-    file_permission = "0400"
+  content         = tls_private_key.bastion_key.private_key_pem
+  filename        = "bastion-key.pem"
+  file_permission = "0400"
 }
+
+# Security Group for Bastion
 
 resource "aws_security_group" "bastion_sg" {
-    name = "bastion-sg"
-    vpc_id = module.vpc.vpc_id
+  name   = "bastion-sg"
+  vpc_id = module.vpc.vpc_id
 
-    ingress {
-        description = "SSH from my IP"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
-    }
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 
-    tags = {
-        Name = "bastion-sg"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-sg"
+  }
 }
 
+
+# Bastion Host
+
 module "bastion_host" {
-    source = "terraform-aws-modules/ec2-instance/aws"
-    name = "bastion-host"
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t3.micro"
-    key_name = "aws_key_pair.bastion_keypair.public_key"
-    monitoring = true
-    subnet_id = element(module.vpc.public_subnets, 0)
-    vpc_security_group_ids = [aws_security_group.bastion_sg.id]
-    associate_public_ip_address = true
-    tags = {
-        Terraform = "true"
-        Environment = "DEV"
-        Role = "bastion"
-    }
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "bastion-host"
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.bastion_keypair.key_name
+  monitoring    = true
+
+  subnet_id              = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Role        = "bastion"
+  }
 }
